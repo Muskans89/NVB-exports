f1fc410e4100acf62ef244e46f74bf8546a13b30
Muskans89 <109502928+Muskans89@users.noreply.github.com>
2024-11-14 11:57:56 +0530

    Add files via upload

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -5,6 +5,7 @@ import bannerImage from '../assets/neon2.jpg'; // Import the banner image
 import contactImage from '../assets/party.jpg'; // Import the contact image
 import { motion } from 'framer-motion'; // Import motion from framer-motion
 import HoliCursorEffect from '../components/holicoloreffect';
+import axios from 'axios';
 
 const ContactPage = () => {
   const [formData, setFormData] = useState({
@@ -22,54 +23,55 @@ const ContactPage = () => {
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     
-    // Check if all required fields are filled out
     if (!formData.firstName || !formData.lastName || !formData.email || !formData.message) {
       setErrorMessage('Please fill in all the required fields.');
-    } else {
-      setErrorMessage('');
-      console.log('Form Data Submitted:', formData);
-      // Handle form submission logic here (like sending data to the server)
+      return;
+    }
+
+    setErrorMessage('');
+
+    try {
+      await axios.post('http://localhost:3001/send-email', formData);
+      alert('Email sent successfully');
+      setFormData({ firstName: '', lastName: '', email: '', message: '' }); // Clear the form
+    } catch (error) {
+      console.error('Error sending email:', error);
+      alert('Failed to send email');
     }
   };
 
   // Function to handle scroll events
   const handleScroll = () => {
     const section = document.getElementById('contact-section');
-    if (section) { // Check if section is not null
+    if (section) {
       const { top, bottom } = section.getBoundingClientRect();
-      // Check if the section is in the viewport
       if (top < window.innerHeight && bottom >= 0) {
         setIsVisible(true);
-        window.removeEventListener('scroll', handleScroll); // Remove event listener after visibility is set
+        window.removeEventListener('scroll', handleScroll);
       }
     }
   };
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll); // Clean up the event listener
+    return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
   return (
     <div className="font-poppins bg-gray-50">
-      {/* Header Component */}
       <Header />
 
       {/* About Us Banner Section */}
       <motion.section 
         className="relative h-80 md:h-96 lg:h-[500px] flex items-center justify-center bg-cover bg-center"
-        style={{ 
-          backgroundImage: `url(${bannerImage})`,
-          backgroundAttachment: 'fixed' // Fix the background
-        }}
-        initial={{ opacity: 0 }} // Starting state
-        animate={{ opacity: 1 }} // Ending state
-        transition={{ duration: 1 }} // Transition duration
+        style={{ backgroundImage: `url(${bannerImage})`, backgroundAttachment: 'fixed' }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
       >
-        {/* Dark Overlay */}
         <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
           <h1 className="text-white text-xl md:text-4xl font-bold" style={{ fontFamily: 'The Seasons' }}>Contact Us</h1>
         </div>
@@ -79,9 +81,9 @@ const ContactPage = () => {
       <motion.section 
         id="contact-section"
         className="py-12 px-4 md:px-8 max-w-screen-lg mx-auto flex flex-col md:flex-row gap-8"
-        initial={{ opacity: 0 }} // Starting state
-        animate={isVisible ? { opacity: 1 } : {}} // Animate based on visibility
-        transition={{ duration: 1 }} // Transition duration
+        initial={{ opacity: 0 }}
+        animate={isVisible ? { opacity: 1 } : {}}
+        transition={{ duration: 1 }}
       >
         {/* Left Column - Contact Information */}
         <div className="flex-1 p-6 flex flex-col justify-between">
@@ -94,11 +96,7 @@ const ContactPage = () => {
           
           {/* Image Below Contact Details */}
           <div className="mt-4">
-            <img 
-              src={contactImage} 
-              alt="Contact Visual" 
-              className="w-full h-64 object-cover" // Ensures the image takes full width and specified height without rounding
-            />
+            <img src={contactImage} alt="Contact Visual" className="w-full h-64 object-cover" />
           </div>
         </div>
 
@@ -107,7 +105,7 @@ const ContactPage = () => {
           <form onSubmit={handleSubmit}>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
               <div>
-                <label htmlFor="firstName" className="block text-sm  mb-2" style={{ fontFamily: 'The Seasons' }}>First Name</label>
+                <label htmlFor="firstName" className="block text-sm mb-2" style={{ fontFamily: 'The Seasons' }}>First Name</label>
                 <input
                   type="text"
                   id="firstName"
@@ -132,7 +130,7 @@ const ContactPage = () => {
               </div>
             </div>
             <div className="mb-4">
-              <label htmlFor="email" className="block text-sm  mb-2" style={{ fontFamily: 'The Seasons' }}>Email </label>
+              <label htmlFor="email" className="block text-sm mb-2" style={{ fontFamily: 'The Seasons' }}>Email</label>
               <input
                 type="email"
                 id="email"
@@ -144,7 +142,7 @@ const ContactPage = () => {
               />
             </div>
             <div className="mb-4">
-              <label htmlFor="message" className="block text-sm  mb-2" style={{ fontFamily: 'The Seasons' }}>Message </label>
+              <label htmlFor="message" className="block text-sm mb-2" style={{ fontFamily: 'The Seasons' }}>Message</label>
               <textarea
                 id="message"
                 name="message"
@@ -168,24 +166,24 @@ const ContactPage = () => {
       {/* Google Map Section */}
       <motion.section 
         className="py-6 px-2 md:px-8 max-w-screen-lg mx-auto"
-        initial={{ opacity: 0 }} // Starting state
-        animate={isVisible ? { opacity: 1 } : {}} // Animate based on visibility
-        transition={{ duration: 1 }} // Transition duration
+        initial={{ opacity: 0 }}
+        animate={isVisible ? { opacity: 1 } : {}}
+        transition={{ duration: 1 }}
       >
-        <h2 className="bg-red-500 text-white  text:xl md:text-2xl font-bold mb-4 text-center" style={{ fontFamily: 'The Seasons' }}>Find Us Here</h2>
+        <h2 className="bg-red-500 text-white text-xl md:text-2xl font-bold mb-4 text-center" style={{ fontFamily: 'The Seasons' }}>Find Us Here</h2>
         <div className="p-4 rounded-lg shadow-lg">
           <iframe 
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d97641.88998473445!2d73.80483282905544!3d18.52506179992735!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2cbb0e1c0b6d5%3A0x854fc793b3db5858!2sSujay%20Garden%2C%20Mukund%20Nagar%2C%20Pune%2C%20Maharashtra%20411037!5e0!3m2!1sen!2sin!4v1679992490176!5m2!1sen!2sin"
             width="100%"
-            height="300" // Increased height for better visibility
+            height="300"
             style={{ border: 0 }}
             allowFullScreen
             loading="lazy"
           ></iframe>
         </div>
       </motion.section>
-      <HoliCursorEffect/>
-      {/* Footer Component */}
+      
+      <HoliCursorEffect />
       <Footer />
     </div>
   );
